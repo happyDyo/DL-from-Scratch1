34b955bc805a0cabfd0fbdba4848925fa1c56b8c
daeyong <ydy9201@naver.com>
2026-02-12 13:21:32 +0900

    practice until 4.2.2

--- ch04_NN_Training/s4_2_Loss_function/cee.py
+++ ch04_NN_Training/s4_2_Loss_function/cee.py
@@ -5,3 +5,15 @@ import numpy as np
 def cross_entropy_error(y, t) :
     delta = 1e-7
     return -np.sum(t * np.log(y + delta))
+
+if __name__ == "__main__" :
+    t = [0,0,1,0,0,0,0,0,0,0]
+    y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+    r1 = cross_entropy_error(np.array(y), np.array(t))
+
+    y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+    r2 = cross_entropy_error(np.array(y), np.array(t))
+
+    print(f"정답은 2 입니다.")
+    print(f"2라고 추정했을 때 교차엔트로피오차 : {r1}")
+    print(f"7이라고 추정했을 때 교차엔트로피오차 : {r2}")
