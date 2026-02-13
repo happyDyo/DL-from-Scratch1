7795fc5d9a904e033d60f45807bb179033d8094d
DDiyong00 <ydy9201@naver.com>
2026-02-13 14:47:12 +0900

    practice until 4.2.3

--- ch04_NN_Training/s4_2_Loss_function/minibatch.py
+++ ch04_NN_Training/s4_2_Loss_function/minibatch.py
@@ -1,2 +1,20 @@
 # 4.2.3 미니배치 학습
 
+import numpy as np
+from dataset.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(normalize=True, one_hot_label=True)
+
+print(x_train.shape)
+print(t_train.shape)
+
+train_size = x_train.shape[0]
+batch_size = 10
+batch_mask = np.random.choice(train_size, batch_size)
+x_batch = x_train[batch_mask]
+t_batch = t_train[batch_mask]
+
+print(f"batch_mask = {batch_mask}")
+print(f"x_batch의 shape = {x_batch.shape}")
+print(f"t_batch = {t_batch}")
