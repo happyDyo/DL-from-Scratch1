8bfa85c05eade58932054990c8c5242920ce0326
DDiyong00 <ydy9201@naver.com>
2026-02-19 18:32:42 +0900

    practice until partial_diff1

--- ch04_NN_Training/s4_3_Numerical_Differentiation/partial_diff.py
+++ ch04_NN_Training/s4_3_Numerical_Differentiation/partial_diff.py
@@ -7,16 +7,16 @@ def function(x) :           # 변수가 여러개임에 주목
     return np.sum(x**2, axis = 0)
 
 if __name__ == "__main__" :
-    x0 = np.arange(-3.0, 3.0, 0.1)
-    x1 = np.arange(-3.0, 3.0, 0.1)
-    x0, x1 = np.meshgrid(x0, x1)
+    x0_line = np.arange(-3.0, 3.0, 0.1)
+    x1_line = np.arange(-3.0, 3.0, 0.1)
+    x0, x1 = np.meshgrid(x0_line, x1_line)
     x = np.array([x0,x1])
     z = function(x)
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     
-    ax.plot_surface(x[0], x[1], z, cmap='viridis')
+    ax.plot_surface(x0, x1, z, cmap='viridis')
 
     ax.set_xlabel("x0")
     ax.set_ylabel("x1")
