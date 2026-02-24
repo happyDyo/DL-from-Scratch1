062fdf0d374060a530e466c462a0a50026b9764a
DDiyong00 <ydy9201@naver.com>
2026-02-24 09:01:22 +0900

    practice until gradient function

--- ch04_NN_Training/s4_4_Gradient/gd_example.py
+++ ch04_NN_Training/s4_4_Gradient/gd_example.py
@@ -10,16 +10,16 @@ if __name__ == "__main__" :
     init_x = np.array([-3.0, 4.0])
     print(f"초기 x : {init_x}\n")
 
-    print(f"학습률 클 때, lr=10.0")
+    print(f"학습률 클 때, lr=10.0")         # 발산할 것
     final = gradient_descent(function_2, init_x, 10.0, 100)
     print(f"최소값의 좌표 : {final}")
 
-    print(f"적당한 학습률, lr=0.1")
+    print(f"적당한 학습률, lr=0.1")         # 정확한 결과
     init_x = np.array([-3.0, 4.0])
     final = gradient_descent(function_2, init_x, 0.1, 100)
     print(f"최소값의 좌표 : {final}")
 
-    print(f"학습률이 작을 때, lr=1e-10")
+    print(f"학습률이 작을 때, lr=1e-10")    # 낮은 갱신률
     init_x = np.array([-3.0, 4.0])
     final = gradient_descent(function_2, init_x, 1e-10, 100)
     print(f"최소값의 좌표 : {final}")
