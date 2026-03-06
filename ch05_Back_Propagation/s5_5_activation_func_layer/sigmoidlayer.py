2b4fed5c104a3e24e20be82a3e5f2949cd31e401
DDiyong00 <ydy9201@naver.com>
2026-03-07 08:25:41 +0900

    practice until Sigmoid Layer

---
+++ ch05_Back_Propagation/s5_5_activation_func_layer/sigmoidlayer.py
@@ -0,0 +1,18 @@
+# 5.5.2 Sigmoid 계층
+
+import numpy as np
+
+class Sigmoid :
+    def __init__(self) :
+        self.out = None
+
+    def forward(self, x) :
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+
+        return out
+    
+    def backward(self, dout) :
+        dx = dout * self.out * (1 - self.out)
+
+        return dx
