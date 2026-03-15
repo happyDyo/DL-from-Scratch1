56f34123577ce3b903562470f3f66ffe04d99536
DDiyong00 <ydy9201@naver.com>
2026-03-15 12:32:51 +0900

    practice until distribution of activations1

---
+++ ch06_Learning_Techniques/s6_2_initial_weight/hlayer_distribution.py
@@ -0,0 +1,32 @@
+# 6.2.2 은닉층의 활성화값 분포
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(x) :
+    return 1 / (1 + np.exp(-x))
+
+x = np.random.randn(1000, 100)      # 1000개의 데이터
+node_num = 100                      # 각 은닉층의 노드(뉴런) 수
+hidden_layer_size = 5               # 은닉층이 5개
+activations = {}                    # 이곳에 활성화 결과(활성화값)를 저장
+
+for i in range(hidden_layer_size) :
+    if i != 0 :
+        x = activations[i-1]
+    w = np.random.randn(node_num, node_num) * 0.25
+    a = np.dot(x, w)
+    z = sigmoid(a)
+    activations[i] = z
+
+for i, a in activations.items() :
+    plt.subplot(1, len(activations), i+1)
+    plt.title(str(i+1) + "-layer")
+    if i != 0 :
+        plt.yticks([], [])
+    plt.hist(a.flatten(), 30, range=(0,1))
+plt.show()
+
+# n = np.arange(-5, 5, 0.1)
+# plt.plot(n, sigmoid(n))
+# plt.show()
