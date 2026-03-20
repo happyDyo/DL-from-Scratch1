6eaf089257ad51f31c69c492398d4be1119a47a3
DDiyong00 <ydy9201@naver.com>
2026-03-20 15:30:25 +0900

    practice until cnn 1

---
+++ ch07_CNN/s7_4_convpool_layer/cnn.py
@@ -0,0 +1,12 @@
+# 7.4.3 합성곱 계층 구현하기
+
+from common.util import im2col
+import numpy as np
+
+x1 = np.random.rand(1, 3, 7, 7)             # (데이터 수, 채널 수, 높이, 너비)
+col1 = im2col(x1, 5, 5, stride=1, pad=0)
+print(col1.shape)
+
+x2 = np.random.rand(10, 3, 7, 7)            # 데이터 10개
+col2 = im2col(x2, 5, 5, stride=1, pad=0)
+print(col2.shape)
