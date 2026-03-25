05e1f9bb751dc248cc3b5ecd5c75dbbb3ad296e9
DDiyong00 <ydy9201@naver.com>
2026-03-25 11:04:32 +0900

    practice until simple CNN 3

--- ch07_CNN/s7_5_CNN/simple_convnet.py
+++ ch07_CNN/s7_5_CNN/simple_convnet.py
@@ -1,6 +1,11 @@
 # 7.5 CNN 구현하기
 
+from ch07_CNN.s7_4_convpool_layer.convolution import Convolution
+from ch07_CNN.s7_4_convpool_layer.pooling import Pooling
+from common.layers import Relu, Affine, SoftmaxWithLoss
+from collections import OrderedDict
 import numpy as np
+
 
 class SimpleConvNet :
     def __init__(self, input_dim=(1, 28, 28),
@@ -16,3 +21,30 @@ class SimpleConvNet :
 
         # 기본적으로 stride = 2 가정 하에 세운 공식
         pool_output_size = int(filter_num * (conv_output_size/2) * (conv_output_size/2))
+
+        self.params = {}
+        self.params['W1'] = weight_init_std * \
+                            np.random.randn(filter_num, input_dim[0],
+                                            filter_size, filter_size)
+        self.params['b1'] = np.zeros(filter_num)
+        self.params['W2'] = weight_init_std * \
+                            np.random.randn(pool_output_size,
+                                            hidden_size)
+        self.params['b2'] = np.zeros(hidden_size)
+        self.params['W3'] = weight_init_std * \
+                            np.random.randn(hidden_size, output_size)
+        self.params['b3'] = np.zeros(output_size)
+
+        self.layers = OrderedDict()
+        self.layers['Conv1'] = Convolution(self.params['W1'],
+                                            self.params['b1'],
+                                            conv_param['stride'],
+                                            conv_param['pad'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
+        self.layers['Affine1'] = Affine(self.params['W2'],
+                                        self.params['b2'])
+        self.layers['Relu2'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W3'],
+                                        self.params['b3'])
+        self.last_layer = SoftmaxWithLoss()
